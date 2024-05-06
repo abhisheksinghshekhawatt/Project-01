be95a87dd50907f1a5b2cfb3abf472e00adb2cd0
abhisheksinghshekhawatt <169075447+abhisheksinghshekhawatt@users.noreply.github.com>
2024-05-06 21:39:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // This function will be executed when the DOM is fully loaded
+    alert('You are welcome in Abhishek"s website');
+});
